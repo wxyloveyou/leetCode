b1f7933bfc36d53fb0f3747fc056722890ea0e3a
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-09-11 10:46:31 +0800

    info

---
+++ src/aa.java
@@ -0,0 +1,10 @@
+/**
+ * @author : WXY
+ * @create : 2022-09-11 10:46
+ * @Info :
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("jel;j");
+    }
+}
