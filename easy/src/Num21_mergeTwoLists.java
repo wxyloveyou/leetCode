29d47f70d10dbde8812ecee0a5bde64e5fbf16ca
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-09-12 21:14:58 +0800

    info leetCode 21. 合并两个有序链表

---
+++ easy/src/Num21_mergeTwoLists.java
@@ -0,0 +1,48 @@
+import org.omg.PortableServer.LIFESPAN_POLICY_ID;
+
+/**
+ * @author : WXY
+ * @create : 2022-09-12 20:12
+ * @Info : leetCode 21. 合并两个有序链表
+ */
+public class Num21_mergeTwoLists {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        }
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return head.next;
+    }
+
+}
