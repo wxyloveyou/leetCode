4cd3a726a2d9761f774a7eb879b30fefb204e7f2
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-09-12 21:35:24 +0800

    info leetCode 26. 删除有序数组中的重复项

---
+++ easy/src/Num26_removeDuplicates.java
@@ -0,0 +1,18 @@
+/**
+ * @author : WXY
+ * @create : 2022-09-12 21:16
+ * @Info : leetCode 26. 删除有序数组中的重复项
+ */
+public class Num26_removeDuplicates {
+    public static int removeDuplicates(int[] nums) {
+        int[] arr = new int[nums.length];
+        int index = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (nums[i-1] != nums[i]) {
+                nums[++index] = nums[i];
+            }
+        }
+        return index+1;
+    }
+}
