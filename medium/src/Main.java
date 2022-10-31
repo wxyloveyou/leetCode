81edd3cabc09d7ee5cf7c1454471f9172e104afd
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-10-31 22:43:06 +0800

    info

---
+++ medium/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author : WXY
+ * @create : ${YEAR}-${MONTH}-${DAY} ${TIME}
+ * @Info :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
