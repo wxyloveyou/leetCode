45160be6283c9d0fa91d15c3f96bbf644fc5a2ee
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-11-07 17:22:13 +0800

    info leetCode : 238. 除自身以外数组的乘积

---
+++ arrays/src/Num238_productExceptSelf.java
@@ -0,0 +1,12 @@
+/**
+ * @author : WXY
+ * @program : LeetCode
+ * @className ：Num238_
+ * @create : 2022/11/7:17:20
+ * @info : 238. 除自身以外数组的乘积
+ **/
+public class Num238_productExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        return new int[]{1};
+    }
+}
