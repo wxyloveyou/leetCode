4532d0429fe52b9d855776b4fd17b238181e2c3e
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-11-07 17:33:13 +0800

    info leetCode : 238. 除自身以外数组的乘积

--- arrays/src/Num238_productExceptSelf.java
+++ arrays/src/Num238_productExceptSelf.java
@@ -7,6 +7,18 @@
  **/
 public class Num238_productExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
-        return new int[]{1};
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = left;
+            left = left * nums[i];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right = right * nums[i];
+        }
+        return res;
     }
 }
