21ad9ac358478de300bbe125f9f0ed29b6944047
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-11-14 19:49:05 +0800

    info LeetCode : 67. 二进制求和

--- A_String/src/Num67_addBinary.java
+++ A_String/src/Num67_addBinary.java
@@ -13,29 +13,32 @@ import java.io.InputStream;
 
 public class Num67_addBinary {
     public static String addBinary(String a, String b) {
-
-        int aNum = conserve(a);
-        int bNum = conserve(b);
-        int count = aNum + bNum;
-//        int aNum = Integer.parseInt(a);
-//        int bNum = Integer.parseInt(b);
-
-        System.out.println(count);
-        return "";
-    }
-
-    private static int conserve(String a) {
-        char[] asrt = a.toCharArray();
-        int aNum = 0;
-        for (int i = asrt.length - 1; i >= 0; i--) {
-            if (asrt[i] == '1') {
-                aNum =  aNum + (int) (Math.pow(2, asrt.length - i - 1));
+        if(a == null || a.length() == 0) return b;
+        if(b == null || b.length() == 0) return a;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int c = 0; // 进位
+        StringBuilder stb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            if (i >= 0) {
+                c += a.charAt(i--) - '0';
             }
+            if (j >= 0) {
+                c += b.charAt(j--) - '0';
+            }
+            stb.append(c % 2);
+            c >>= 1;
         }
-        return aNum;
+        String s = stb.reverse().toString();
+        return c > 0 ? '1' + s : s;
     }
+
 
     public static void main(String[] args) {
-        addBinary("011", "010");
+        String b = "110";
+        String a = "011";
+        String str = addBinary(a, b);
+        System.out.println("结 果 ：" + str);
+
     }
 }
