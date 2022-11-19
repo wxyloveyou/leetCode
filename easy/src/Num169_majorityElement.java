f62e2cce3a4b950075e3486b6c25a30121567439
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-11-19 08:45:35 +0800

    info LeetCode ： 169. 多数元素

--- easy/src/Num169_majorityElement.java
+++ easy/src/Num169_majorityElement.java
@@ -10,6 +10,7 @@ public class Num169_majorityElement {
 
     public static int majorityElement(int[] nums) {
         int c = 0;
+        int ans = 0;
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (count == 0) {
