fcd54eed40cd18b0e4685e1807f7bf6b7675b1fe
WXY <2423144591@qq.com>
2022-11-28 14:09:00 +0800

    info leetCode : 400. 第 N 位数字
    Signed-off-by: WXY <2423144591@qq.com>

---
+++ A_NumAndBit/src/Num400_findNthDigit.java
@@ -0,0 +1,19 @@
+/**
+ * @author : WXY
+ * @program : LeetCode
+ * @className ：Num400_
+ * @create : 2022/11/27:10:35
+ * @info :400. 第 N 位数字
+ **/
+public class Num400_findNthDigit {
+    public static int findNthDigit(int n) {
+      return 0;
+    }
+
+    public static void main(String[] args) {
+        int nthDigit = findNthDigit(3);
+        System.out.println(nthDigit);
+
+    }
+
+}
